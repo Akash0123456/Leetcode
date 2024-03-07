5ce4cecbe478740e199ad15bacde894113d21b7f
Akash <agampala@ufl.edu>
2024-03-06 21:04:11 -0600

    Leetcode 15

---
+++ 15.py
@@ -0,0 +1,58 @@
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+"""
+
+def threeSum(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    res = []
+    # Sort nums list
+    nums.sort()
+
+    for i, num in enumerate(nums):
+        if i > 0 and num == nums[i-1]:
+            continue
+        
+        l = i+1
+        r = len(nums)-1
+
+        while l < r:
+            sum_val = num + nums[l] + nums[r]
+
+            if sum_val > 0:
+                r = r-1
+            elif sum_val < 0:
+                l=l+1
+            else:
+                res.append([num, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l-1] and l < r:
+                    l +=1
+        
+    
+    return res
